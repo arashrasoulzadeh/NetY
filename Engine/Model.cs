9830df46407d1e3dbb98760908640e8de54dab6c
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2018-06-12 04:39:00 +0430

    comments

---
+++ Engine/Model.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Model  {
+    public string toString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+NetY is a thread-safe Rest Client For Unity
+
+Documentation is comming soon.
